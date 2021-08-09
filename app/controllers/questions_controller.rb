cfe362bc169d8d7a4e937e2f612e49ca7700850f
adrien leclercq <leclercq.adrien96@gmail.com>
2021-08-09 12:53:11 +0200

    Stupid Exo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Class controller
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @answer = coach_answer(params[:question])
+  end
+
+  private
+
+  def coach_answer(question)
+    if question.downcase == 'I am going to work right now!'
+      ''
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
